67a6e15b9b8215907a289a4b0dd5cf13646ba85b
Andrea Darrah <andreadarrah@MacBook-Air.local>
2022-04-12 16:29:06 -0500

    added queries, updated schema

--- schema.sql
+++ schema.sql
@@ -44,6 +44,17 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title)
 );
 
+DROP TABLE titles;
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
@@ -53,4 +64,51 @@ CREATE TABLE dept_emp(
 	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no, to_date)
+);
+
+
+--DROP TABLE IF EXISTS
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES  departments(dept_no),
+	PRIMARY KEY (emp_no)
+);
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES  departments(dept_no),
+	PRIMARY KEY (emp_no)
+);
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM dept_emp;
